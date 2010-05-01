b6946a78d6909e777a6f2ff86d40cf6c69609d64
Johannes Held <mail@hehejo.de>
2010-05-01 10:09:17 +0200

    Listings mit Ausgabe

--- listings/init.sh
+++ listings/init.sh
@@ -1,5 +1,9 @@
 $ mkdir FUN; cd FUN
-$ git init .
-$ vim main.cc
-$ git add main.cc
+$ git init
+Initialized empty Git repository in /home/jo/FUN/.git/
+$ vim README
+$ git add README
 $ git commit -m "nice message"
+[master (root-commit) 90dd184] nice message
+ 1 files changed, 1 insertions(+), 0 deletions(-)
+ create mode 100644 README
